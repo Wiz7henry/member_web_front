cc8bf051a400981dc1188ff8f0d5d5eb37b148ee
Wiz7henry <std.66122710102@ubru.ac.th>
2025-02-16 22:09:00 +0700

    first commit

--- src/components/globals.js
+++ src/components/globals.js
@@ -1,4 +1,4 @@
-const BASE_API_URL = "https://members-web.onrender.com";
+const BASE_API_URL = "https://backend-members.onrender.com";
 // const BASE_API_URL = "http://10.255.4.113:5000";
 const APP_NAME = "Member Management System";
 export { BASE_API_URL, APP_NAME };
